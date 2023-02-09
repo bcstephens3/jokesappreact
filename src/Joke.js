5a9821701c5005dbc4c301c334150cfd109dbe0f
Brandon Stephens <bcstephens3@crimson.ua.edu>
2023-02-09 07:44:40 -0600

    stuff

--- src/Joke.js
+++ src/Joke.js
@@ -1,12 +1,26 @@
+import React from "react"
+
 export default function Joke(props) {
+
+  const [isShown, setIsShown] = React.useState(false)
+
+
+  function toggle(){
+    setIsShown(prevShown => !prevShown) 
+  }
+
+ 
+
   return (
     <div>
         <h1>
             Joke: {props.setup}
         </h1>
         <p>
-            A: {props.punchl}
+            A: {isShown ? props.punchl : ""}
         </p>
+        <h1>{isShown ? "Shown" : "Not shown"}</h1>
+        <button className="toggle-btn" onClick={toggle}>{isShown ? "Hide":"Show"} PL</button>
     </div>
   );
 }
